6204f4be3082081b6d5a44e1ae1438c923676c8f
Lee Pollok <lee.pollok@gmail.com>
2021-10-21 15:30:28 -0700

    final step of test solved

--- hiringsolved_end.py
+++ hiringsolved_end.py
@@ -3,12 +3,21 @@ import requests
 
 
 url = 'https://challenge.hiringsolved.com/038FD8'
-payload = {'hirable': 'true',
-           'name': 'Lee Pollok',
-           'email': 'lee.pollok@gmail.com',
-           'phone': '6026792805',
-           'twitter': 'https://twitter.com/LeePollok'}
+name = 'Lee Pollok'
+email = 'lee@pollok.io'
+phone = '6026792805'
+twitter = 'https://twitter.com/LeePollok'
+
+final_url = '&'.join([
+    url,
+    'hirable=true',
+    'name=' + name,
+    'email=' + email,
+    'phone=' + phone,
+    'twitter=' + twitter])
+print(final_url)
+'''
 response = requests.post(url, data=json.dumps(payload))
 print(response)
 print(response.text)
-
+'''
